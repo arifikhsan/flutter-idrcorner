474ab4ea36187b092f5c3c23e008a7d33d263fec
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-04-24 11:51:43 +0700

    app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: HalamanSaya(),
+    title: "Tanyain Application",
+  ));
+}
+
+class HalamanSaya extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Center(
+          child: Text("TANYAIN"),
+        ),
+        actions: <Widget>[Icon(Icons.search), Icon(Icons.security)],
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
