97fe2a797da19b45d56eb0cb0b1d09bd39a7c1e5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-04-24 13:10:02 +0700

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: HalamanSaya(),
-    title: "Tanyain Application",
+    title: "IDR Corner Application",
   ));
 }
 
@@ -14,12 +14,27 @@ class HalamanSaya extends StatelessWidget {
       appBar: AppBar(
         leading: Icon(Icons.home),
         title: Center(
-          child: Text("TANYAIN"),
+          child: Text("IDR Corner"),
         ),
         actions: <Widget>[Icon(Icons.search), Icon(Icons.security)],
         backgroundColor: Colors.red,
       ),
       backgroundColor: Colors.yellow,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Icon(Icons.local_pizza, size: 70.0, color: Colors.red),
+            Row(
+              children: <Widget>[
+                Icon(Icons.donut_large, size: 70.0, color: Colors.red),
+                Icon(Icons.donut_large, size: 70.0, color: Colors.red),
+                Icon(Icons.donut_large, size: 70.0, color: Colors.red)
+              ],
+            ),
+            Icon(Icons.cake, size: 70.0, color: Colors.red)
+          ],
+        ),
+      ),
     );
   }
 }
